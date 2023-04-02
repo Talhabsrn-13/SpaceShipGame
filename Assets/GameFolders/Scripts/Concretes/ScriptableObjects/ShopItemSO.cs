c2835a50fd2039624a0acc99672b1561ddc458af
TalhaBaşaran <tbsrn7@gmail.com>
2023-04-03 01:44:34 +0300

    3. gemi değiştirildi
    3. gemi değiştirldi ve shopItem So güncellendi.

--- Assets/GameFolders/Scripts/Concretes/ScriptableObjects/ShopItemSO.cs
+++ Assets/GameFolders/Scripts/Concretes/ScriptableObjects/ShopItemSO.cs
@@ -16,6 +16,27 @@ namespace Space.SO
         public int priceMultiplier;
         public int damageMultiplier;
         public bool ownership;
-    }
 
+        public void SaveData()
+        {
+            PlayerPrefs.SetInt(title+ "ItemDamage", damage);
+            PlayerPrefs.SetInt(title+"ItemPrice", price);
+            PlayerPrefs.SetInt(title + "ItemLevel", level);
+            PlayerPrefs.SetInt(title + "ItemPriceMultiplier", priceMultiplier);
+            PlayerPrefs.SetInt(title + "ItemDamageMultiplier", damageMultiplier);
+            PlayerPrefs.SetInt(title + "ItemOwnership", ownership ? 1 : 0);
+            PlayerPrefs.Save();
+        }
+
+        public void LoadData()
+        {
+            damage = PlayerPrefs.GetInt(title + "ItemDamage", damage);
+            price = PlayerPrefs.GetInt(title + "ItemPrice", price);
+            level = PlayerPrefs.GetInt(title + "ItemLevel", level);
+            priceMultiplier = PlayerPrefs.GetInt(title + "ItemPriceMultiplier", priceMultiplier);
+            damageMultiplier = PlayerPrefs.GetInt(title + "ItemDamageMultiplier", damageMultiplier);
+            ownership = PlayerPrefs.GetInt(title + "ItemOwnership", ownership ? 1 : 0) == 1;
+        }
+    }
+ 
 }
